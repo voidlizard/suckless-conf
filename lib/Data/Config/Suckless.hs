1f973384cb70fe579c751aa4aed692af17300cf7
Dmitry Zuikov <dzuikov@gmail.com>
2023-02-07 12:49:04 +0300

    wip

---
+++ lib/Data/Config/Suckless.hs
@@ -0,0 +1,9 @@
+module Data.Config.Suckless
+  ( module Data.Config.Suckless.Syntax
+  , module Data.Config.Suckless.Parse
+  )
+  where
+
+import Data.Config.Suckless.Syntax
+import Data.Config.Suckless.Parse
+
